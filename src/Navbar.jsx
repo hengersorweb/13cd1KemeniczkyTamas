d1ce50845fd521356c0a6009536a6bf9f1eced21
KTomi005 <kemtam197@hengersor.hu>
2025-02-17 12:16:22 +0100

    Menu elkészítése

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = (() => {
 
     return(
         <div>
-            <nav>
-                <Link to="/">Főoldal </Link>
-                <NavLink to="/ermesek">Az olimpia érmesei </NavLink>
-                <NavLink to="/mermesek">A magyar érmesek </NavLink>
-
-
+            
+            <nav className="dt w-13 border-box pa3 ph5-ns bg-light-gray">
+                <Link className="dtc v-mid mid-gray link dim w-25 " to='/'>Főoldal </Link>
+                <NavLink className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" to='/ermesek'>Az olimpia érmesei </NavLink>
+                <NavLink className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" to='/mermesek'>A magyar érmesek </NavLink>
             </nav>
+            
         </div>
     )
 }
