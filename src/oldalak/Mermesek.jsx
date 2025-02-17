b0515baf1e5b92d6ddab2522117d2ced1cdb9c8b
KTomi005 <kemtam197@hengersor.hu>
2025-02-17 11:29:09 +0100

    szükséges modulok import
    bootstrap,router, tachyons

---
+++ src/oldalak/Mermesek.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Mermesek = (() => {
+
+    return(
+        <div>
+            <h1>Az olimpia magyar érmesei</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi expedita at cumque asperiores aspernatur repellat aliquam harum quod deserunt, ullam, officia praesentium atque alias perferendis inventore, ex beatae necessitatibus? Minima, deleniti adipisci maiores labore hic, reiciendis cum exercitationem officia quia possimus neque dolorem commodi quasi mollitia debitis temporibus maxime necessitatibus qui pariatur provident amet, dignissimos ab. Repudiandae nihil, eligendi molestias eos quidem veniam magnam obcaecati veritatis tenetur possimus ducimus natus officia sit excepturi molestiae velit, nesciunt facere ad ullam sed! Facere, sapiente totam? Magni error eligendi labore vero, nemo eum totam, at adipisci quae autem itaque earum, dolore dicta est.</p>
+        </div>
+    )
+}
+)
+
+export default Mermesek
